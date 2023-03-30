e73ec6560688ba77ca5a265abab0506cea3ed782
sebastiansukiennik01 <sebastiansukiennik01@gmail.com>
2023-03-30 23:56:03 +0200

    implement cleaning, normalizing and adding additional variables to data in DataSet class

--- src/preprocessing/dataset.py
+++ src/preprocessing/dataset.py
@@ -1,14 +1,19 @@
 """
 Contains Dataset class used for dividing data to specific datasets.
 """
+from __future__ import annotations
+
 import pandas as pd
 import tensorflow as tf
+import numpy as np
 
 from tensorflow.data import Dataset
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 from dataclasses import dataclass
 from typing import Union
 
+from src.utils.utils import add_variables
 
 @dataclass
 class DataSet(object):
@@ -20,11 +25,11 @@ class DataSet(object):
             all values have to sum up to 1, divides data to [train, test] if 2 values provided
             and to [train, validation, test] if 3 values provided.
     """
-    
+
     data: pd.DataFrame
     ratios: list[float]
     label: Union[str, list[str]]
-    
+
     def __post_init__(self):
         """
         Divides data into train, validation, test sets using specified ratios
@@ -32,7 +37,7 @@ class DataSet(object):
         n = self.data.shape[0]
         train_idx = int(n * self._train_r)
         self.train = self.data.iloc[:train_idx].copy()
-        
+
         if self._validation_r:
             val_idx = int(n * self._validation_r) + train_idx
             self.validation = self.data.iloc[train_idx:val_idx].copy()
@@ -40,7 +45,7 @@ class DataSet(object):
         else:
             self.validation = pd.DataFrame()
             self.test = self.data.iloc[train_idx:].copy()
-            
+
     @property
     def ratios(self) -> list[float]:
         return self._ratios
@@ -59,12 +64,11 @@ class DataSet(object):
             self._validation_r = None
         else:
             self._train_r, self._validation_r, self._test_r = values
-            
-            
+
     @property
     def label(self) -> list[str]:
         return self._label
-        
+
     @label.setter
     def label(self, values: Union[str, list[str]]) -> None:
         self._label = [values] if isinstance(values, str) else values
@@ -73,18 +77,16 @@ class DataSet(object):
         """
         Returns train, validation and test datasets as pandas dataframes.
         """
-        train_x, train_y = self.get_train(type="dataframe")
-        validation_x, validation_y = self.get_validation(type="dataframe")
-        test_x, test_y = self.get_test(type="dataframe")
+
+        return self.get_train(type="dataframe"), self.get_validation(type="dataframe"), self.get_test(type="dataframe")
         
-        return train_x, train_y, validation_x, validation_y, test_x, test_y
 
     def get_tvt_dataset(self) -> tuple[Dataset]:
         """
         Returns train, validation and test datasets as tensorflow datasets.
         """
         # TODO implement conversion to tensorflow datasets
-        return 
+        return
 
     def get_train(self, type: str) -> Union[pd.DataFrame, Dataset]:
         """
@@ -94,7 +96,7 @@ class DataSet(object):
         returns : train dataset
         """
         if type == "dataframe":
-            return self.train.drop(columns=self.label), self.train[self.label]
+            return self.train
         elif type == "dataset":
             # TODO
             ...
@@ -107,9 +109,9 @@ class DataSet(object):
         returns : validation dataset
         """
         if not self._validation_r:
-            return pd.DataFrame(), pd.DataFrame()
+            return pd.DataFrame()
         if type == "dataframe":
-            return self.validation.drop(columns=self.label), self.validation[self.label]
+            return self.validation
         elif type == "dataset":
             # TODO
             ...
@@ -122,7 +124,62 @@ class DataSet(object):
         returns : test dataset
         """
         if type == "dataframe":
-            return self.test.drop(columns=self.label), self.test[self.label]
+            return self.test
         elif type == "dataset":
             # TODO
             ...
+
+    def add_variables(self) -> DataSet:
+        """
+        Adds variables to all datasets (train, validation and test)
+        """
+        self.train = add_variables(self.train)
+        self.validation = add_variables(self.validation)
+        self.test = add_variables(self.test)
+        
+        return self
+    
+    def normalize(self, how: str, features_only: bool = True) -> DataSet:
+        """
+        Performs normalization on all datasets. Categorical features are ommited and left as is.
+        args:
+            how : type of normalization [standarize/min-max]
+        """
+        scaler = {
+            "standarize": StandardScaler,
+            "min-max": MinMaxScaler
+        }.get(how, "min-max")
+        
+        to_scale = set(self.train.columns).difference(self._binary_columns())
+        to_scale = to_scale.difference(self.label) if features_only else to_scale
+        to_scale = list(to_scale)
+        
+        self.train[to_scale] = scaler().fit_transform(self.train[to_scale])
+        self.validation[to_scale] = scaler().fit_transform(self.validation[to_scale])
+        self.test[to_scale] = scaler().fit_transform(self.test[to_scale])
+        
+        return self
+    
+    def clean_data(self) -> DataSet:
+        """
+        Cleanes dataframe from invalid values.
+        """
+        self.train = self.train.replace([np.inf, -np.inf], np.nan).loc[
+            ~self.train.isna().any(axis=1), :
+        ]
+        self.validation = self.validation.replace(
+            [np.inf, -np.inf], np.nan
+        ).loc[~self.validation.isna().any(axis=1), :]
+        self.test = self.test.replace([np.inf, -np.inf], np.nan).loc[
+            ~self.test.isna().any(axis=1), :
+        ]
+
+        return self
+
+    def _binary_columns(self) -> pd.DataFrame:
+        """
+        Drops binary columns form dataframe
+        """
+        binary = [c for c in self.train if set(self.train[c].unique()) == set([1, 0])]
+        
+        return binary
